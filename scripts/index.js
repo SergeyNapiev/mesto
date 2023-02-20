8bf6d29730a6e4d46970f5f5dab7b6c74d7e3cc4
Sergey Napiev <napiev@mail.ru>
2023-02-20 16:00:59 +0300

    ПР5 лайки, попап создать, плавне закрывания

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,39 @@
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+]; 
+
 const editButton = document.querySelector('.profile__edit-button');
-const popup = document.querySelector('.popup'); 
-const closeButton = document.querySelector('.popup__close');
+const popupEdit = document.querySelector('#edit'); 
+const closeEditButton = document.querySelector('.popup__close');
+
+const addButton = document.querySelector('.profile__add-button');
+const popupAdd = document.querySelector('#add');
+const editHeading = popupAdd.querySelector('.popup__heading');
+const createButton = popupAdd.querySelector('.popup__save');
+const closeAddButton = popupAdd.querySelector('.popup__close');
 
 const formElement = document.querySelector('.popup__form');
 const nameInput = formElement.querySelector('#name');
@@ -8,27 +41,68 @@ const jobInput = formElement.querySelector('#about');
 const nameProfile = document.querySelector('.profile__name');
 const aboutProfile = document.querySelector('.profile__about');
 
-function popupOpen () {
-    popup.classList.add('popup_opened');
+const like = document.querySelector('.elements__heart');
+
+
+function toggleLike () {
+    like.classList.toggle('elements__heart');
+    like.classList.toggle('elements__heart_active');
 }
 
-function popupClose () {
-    popup.classList.remove('popup_opened');
+function popupOpenEdit () {
+    popupEdit.style.animation = 'fadeIn 1s';
+    popupEdit.style.visibility = 'visible';
+    popupEdit.style.transition = '1s';
 }
 
-function handleFormOpen () {
+function popupCloseEdit () {
+    popupEdit.style.animation = 'fadeOut 1s';
+    popupEdit.style.visibility = 'hidden';
+    popupEdit.style.transition = '1s';
+}
+
+function popupOpenAdd () {
+    popupAdd.style.animation = 'fadeIn 1s';
+    popupAdd.style.visibility = 'visible';
+    popupAdd.style.transition = '1s';
+}
+
+function popupCloseAdd () {
+    popupAdd.style.animation = 'fadeOut 1s';
+    popupAdd.style.visibility = 'hidden';
+    popupAdd.style.transition = '1s';
+}
+
+function handleFormOpenEdit () {
     nameInput.value = nameProfile.textContent;
     jobInput.value = aboutProfile.textContent;
-    popupOpen();
+    popupOpenEdit();
 }
 
-function handleFormSubmit (evt) {
+function handleEditFormSubmit (evt) {
     evt.preventDefault();
     nameProfile.textContent = nameInput.value;
     aboutProfile.textContent = jobInput.value;
-    popupClose();
+    nameInput.setAttribute('placeholder', '');
+    jobInput.setAttribute('placeholder', '');
+    popupCloseEdit();
 }
 
-editButton.addEventListener('click', handleFormOpen);
-closeButton.addEventListener('click', popupClose);
-formElement.addEventListener('submit', handleFormSubmit); 
+function handleAddFormOpen () {
+    editHeading.textContent = 'Новое место';
+    nameInput.value = '';
+    jobInput.value = '';
+    nameInput.setAttribute('placeholder', 'Название');
+    jobInput.setAttribute('placeholder', 'Ссылка на картинку');
+    createButton.textContent = 'Создать';
+    popupOpenAdd();
+}
+
+
+
+editButton.addEventListener('click', handleFormOpenEdit);
+closeEditButton.addEventListener('click', popupCloseEdit);
+formElement.addEventListener('submit', handleEditFormSubmit);
+like.addEventListener('click', toggleLike);
+addButton.addEventListener('click', handleAddFormOpen);
+closeAddButton.addEventListener('click', popupCloseAdd);
