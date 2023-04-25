fa759fa78c36ac47423b20ce94b21b0e1613ef60
Sergey Napiev <napiev@mail.ru>
2023-04-26 00:57:48 +0300

    ПР8 полный

---
+++ src/pages/index.js
@@ -0,0 +1,129 @@
+import './index.css';
+import Card  from '../scripts/Card.js';
+import FormValidator from '../scripts/FormValidator.js';
+import Section from '../scripts/Section.js';
+import PopupWithImage from '../scripts/PopupWithImage.js';
+import PopupWithForm from '../scripts/PopupWithForm.js';
+import UserInfo from '../scripts/UserInfo.js';
+
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    },
+]; 
+
+const validationOptions = ({
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__button',
+  inactiveButtonClass: 'popup__button_disabled',
+  inputErrorClass: 'popup__input_type_error',
+  errorClass: 'popup__error_visible',
+  inputSectionsSelector: '.popup__section',
+  inputErrorSelector: '.popup__error'
+});
+
+const container = document.querySelector('.elements');
+
+//
+const popupEdit = document.querySelector('#edit');//
+const popupAdd = document.querySelector('#add');//
+const popupPhoto = document.querySelector('#photo');//
+
+const editButton = document.querySelector('.profile__edit-button');
+const addButton = document.querySelector('.profile__add-button');
+
+
+const editFormElement = document.querySelector('#edit-info');
+const nameInput = editFormElement.querySelector('#name');
+const jobInput = editFormElement.querySelector('#about');
+
+const addFormElement = document.querySelector('#add-place');
+
+// для валидации
+const editFormValidation = new FormValidator(validationOptions, editFormElement);
+editFormValidation.enableValidation();
+
+const addFormValidation = new FormValidator(validationOptions, addFormElement);
+addFormValidation.enableValidation();
+
+//профиль пользователя
+const userInfo = new UserInfo({
+  name: '.profile__name',
+  about: '.profile__about   '
+});
+
+const popupWithEditForm = new PopupWithForm(popupEdit,
+   (name, about) => {
+    userInfo.setUserInfo(name, about);
+    popupWithEditForm.close();
+  }
+);
+
+popupWithEditForm.setEventListeners();
+
+const popupWithAddForm = new PopupWithForm(popupAdd,
+   (item) => {
+    cardZone.addItem(createCard(item));
+  }
+);
+
+popupWithAddForm.setEventListeners();
+
+function createCard(item) {
+  const card = new Card(item, container, handleCardClick);
+  const newCard = card.generateCard();
+  return newCard;
+};
+
+const renderer = (item) => {
+  container.prepend(createCard(item));
+};
+
+function handleCardClick(name, link) {
+  const popupWithImage = new PopupWithImage(popupPhoto);
+  popupWithImage.setEventListeners();
+  popupWithImage.open(name, link);
+};
+
+const cardZone = new Section({items: initialCards, renderer}, container);
+cardZone.renderItems();
+
+function handleOpenEditForm() {
+  const oldUser = userInfo.getUserInfo();
+  nameInput.value = oldUser.name;
+  jobInput.value = oldUser.about;
+  editFormValidation.resetValidation();
+  popupWithEditForm.open();
+}
+
+function handleOpenAddForm() {
+  addFormElement.reset();
+  addFormValidation.resetValidation();
+  popupWithAddForm.open();
+}
+
+editButton.addEventListener('click', handleOpenEditForm);
+
+addButton.addEventListener('click', handleOpenAddForm);
---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: './src/pages/index.js',
+    output: {
+        filename: '[name].[contenthash].js',
+        path: path.join(__dirname, 'build'),
+        clean: true,
+    },
+    devServer: {
+        static: {
+          directory: path.join(__dirname, 'build'),
+        },
+        compress: true,
+        port: 9000,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'style.[contenthash].css',
+        }),
+    ],
+    module: {
+        rules: [
+          {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: [
+                  ['@babel/preset-env', { targets: 'ie 11' }]
+                ]
+              }
+            }
+          },
+          {
+            test: /\.css$/i,
+            use: [
+                MiniCssExtractPlugin.loader,
+                {
+                    loader: 'css-loader',
+                    options: {
+                        importLoaders: 1,
+                    },
+                },
+                'postcss-loader',
+            ],
+          },
+          {
+            test: /\.(png|jpg)/,
+            type: 'asset/resource',
+            generator: {
+                filename: 'img/[name].[contenthash][ext]',
+            }
+          },
+        ]
+    }
+};
