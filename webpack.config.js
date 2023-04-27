874a9e61f5265b5dfb665cbacae9147eeba90df5
Sergey Napiev <napiev@mail.ru>
2023-04-28 00:35:20 +0300

    ПР8 фикс ворнинги dev сборки

--- webpack.config.js
+++ webpack.config.js
@@ -52,7 +52,7 @@ module.exports = {
             ],
           },
           {
-            test: /\.(png|jpg)/,
+            test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
             type: 'asset/resource',
             generator: {
                 filename: 'img/[name].[contenthash][ext]',
